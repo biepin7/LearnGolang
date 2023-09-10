b728fd52521526d9d2c5f4f0f3267a1655084fd7
bp <biepin@qq.com>
2023-09-10 14:40:57 +0800

    sumalltails 的最后一部分

--- LearnGoWithTests/04 Arrays and slices/v4/SumAllTails.go
+++ LearnGoWithTests/04 Arrays and slices/v4/SumAllTails.go
@@ -3,10 +3,21 @@ package main
 func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 
+	// 这种写法，在传入 int[]{}时候，会导致 slice bounds out of range
+	// for _, numbers := range numbersToSum {
+	// 	tail := numbers[1:]
+	// 	sums = append(sums, sum(tail))
+	// }
+
 	for _, numbers := range numbersToSum {
-		tail := numbers[1:]
-		sums = append(sums, sum(tail))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, sum(tail))
+		}
 	}
+
 	return sums
 }
 
